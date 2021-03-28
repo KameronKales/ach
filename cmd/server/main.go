d4d2c74fbd6bd5b061b7ed9825f92370e5fef682
Kam <kameron.kales@banno.com>
2021-03-28 11:34:01 -0400

    this will fix it

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -28,15 +28,17 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/moov-io/ach"
 	"github.com/moov-io/ach/server"
+	"github.com/moov-io/base/admin"
 	"github.com/moov-io/base/http/bind"
 
 	"github.com/go-kit/kit/log"
 )
 
 var (
-	port =  os.Getenv("PORT");
-	httpAddr  = flag.String("port", bind.HTTP("ach"), "HTTP listen address")
+	httpAddr  = flag.String("http.addr", bind.HTTP("ach"), "HTTP listen address")
+	adminAddr = flag.String("admin.addr", bind.Admin("ach"), "Admin HTTP listen address")
 
 	flagLogFormat = flag.String("log.format", "", "Format for log lines (Options: json, plain")
 
@@ -48,6 +50,19 @@ var (
 
 func main() {
 	flag.Parse()
+
+	// Setup logging, default to stdout
+	if v := os.Getenv("LOG_FORMAT"); v != "" {
+		*flagLogFormat = v
+	}
+	if *flagLogFormat == "json" {
+		logger = log.NewJSONLogger(os.Stdout)
+	} else {
+		logger = log.NewLogfmtLogger(os.Stdout)
+	}
+	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+	logger = log.With(logger, "caller", log.DefaultCaller)
+	logger.Log("startup", fmt.Sprintf("Starting ach server version %s", ach.Version))
 
 	// Setup underlying ach service
 	var achFileTTL time.Duration
@@ -76,8 +91,14 @@ func main() {
 	writTimeout, _ := time.ParseDuration("30s")
 	idleTimeout, _ := time.ParseDuration("60s")
 
+
+	// Check to see if our -http.addr flag has been overridden
+	if v := os.Getenv("PORT"); v != "" {
+		*httpAddr = ":" + v
+	}
+
 	serve := &http.Server{
-		Addr:  *httpAddr,
+		Addr:    *httpAddr,
 		Handler: handler,
 		TLSConfig: &tls.Config{
 			InsecureSkipVerify:       false,
@@ -94,11 +115,39 @@ func main() {
 		}
 	}
 
+	// Check to see if our -admin.addr flag has been overridden
+	if v := os.Getenv("PORT"); v != "" {
+		*httpAddr = v
+	}
+	
+
+	// Admin server (metrics and debugging)
+	adminServer := admin.NewServer(*adminAddr)
+	adminServer.AddVersionHandler(ach.Version) // Setup 'GET /version'
+	go func() {
+		logger.Log("admin", fmt.Sprintf("listening on %s", adminServer.BindAddr()))
+		if err := adminServer.Listen(); err != nil {
+			err = fmt.Errorf("problem starting admin http: %v", err)
+			logger.Log("admin", err)
+			errs <- err
+		}
+	}()
+	defer adminServer.Shutdown()
+
 	// Start main HTTP server
 	go func() {
-		if err := serve.ListenAndServe(); err != nil {
-			errs <- err
-			logger.Log("exit", err)
+		if certFile, keyFile := os.Getenv("HTTPS_CERT_FILE"), os.Getenv("HTTPS_KEY_FILE"); certFile != "" && keyFile != "" {
+			logger.Log("startup", fmt.Sprintf("binding to %s for secure HTTP server", *httpAddr))
+			if err := serve.ListenAndServeTLS(certFile, keyFile); err != nil {
+				errs <- err
+				logger.Log("exit", err)
+			}
+		} else {
+			logger.Log("startup", fmt.Sprintf("binding to %s for HTTP server", *httpAddr))
+			if err := serve.ListenAndServe(); err != nil {
+				errs <- err
+				logger.Log("exit", err)
+			}
 		}
 	}()
 
